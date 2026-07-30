fdb0b12f51510a9c307dc90df0e78859242afa0d
Anjula Pulasthi <149922766+anjula-pulasthii@users.noreply.github.com>
2026-07-31 00:39:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,202 @@
+// =====================================
+// HOME
+// =====================================
+
+const startBtn = document.getElementById("startBtn");
+const container = document.querySelector(".container");
+
+// =====================================
+// PAGES
+// =====================================
+
+const permission = document.getElementById("permission");
+const proposal = document.getElementById("proposal");
+const success = document.getElementById("success");
+const maybePage = document.getElementById("maybePage");
+const thanksPage = document.getElementById("thanksPage");
+
+// =====================================
+// PERMISSION BUTTONS
+// =====================================
+
+const permissionYes = document.getElementById("permissionYes");
+const permissionMaybe = document.getElementById("permissionMaybe");
+const permissionAppreciate = document.getElementById("permissionAppreciate");
+const permissionNo = document.getElementById("permissionNo");
+
+// =====================================
+// MOVIE DETAILS
+// =====================================
+
+const movieDate = document.getElementById("movieDate");
+const venue = document.getElementById("venue");
+
+// =====================================
+// MOVIE TICKET
+// =====================================
+
+const ticketDate = document.getElementById("ticketDate");
+const ticketVenue = document.getElementById("ticketVenue");
+
+// =====================================
+// YES / NO BUTTONS
+// =====================================
+
+const confirmBtn =
+document.getElementById("confirmBtn");
+
+// =====================================
+// HOME -> PERMISSION
+// =====================================
+
+startBtn.addEventListener("click", () => {
+
+    container.classList.add("hidden");
+    permission.classList.remove("hidden");
+
+});
+
+// =====================================
+// PERMISSION BUTTONS
+// =====================================
+
+// YES
+
+permissionYes.onclick = () => {
+
+    permission.classList.add("hidden");
+    proposal.classList.remove("hidden");
+
+};
+
+// MAYBE LATER
+
+permissionMaybe.onclick = () => {
+
+    permission.classList.add("hidden");
+    maybePage.classList.remove("hidden");
+
+};
+
+// APPRECIATE
+
+permissionAppreciate.onclick = () => {
+
+    permission.classList.add("hidden");
+    thanksPage.classList.remove("hidden");
+
+};
+
+// =====================================
+// ESCAPING NO BUTTON
+// =====================================
+
+function escapePermissionButton(){
+
+    const x = Math.random()*250 - 125;
+    const y = Math.random()*180 - 90;
+
+    permissionNo.style.transform =
+    `translate(${x}px,${y}px)`;
+
+}
+
+permissionNo.addEventListener("mouseover",escapePermissionButton);
+
+permissionNo.addEventListener("click",(e)=>{
+
+    e.preventDefault();
+
+    escapePermissionButton();
+
+});
+
+// =====================================
+// MOVIE PAGE NO BUTTON
+// =====================================
+
+
+
+// =====================================
+// MOVIE YES
+// =====================================
+
+confirmBtn.onclick=()=>{
+
+    if(
+        movieDate.value==="" ||
+        venue.value===""){
+
+        alert(" Please select Date and Snacks");
+
+        return;
+
+    }
+
+    ticketDate.innerHTML="📅 "+movieDate.value;
+
+    
+    ticketVenue.innerHTML="🍿 "+venue.value;
+
+    proposal.classList.add("hidden");
+
+    success.classList.remove("hidden");
+
+    celebrate();
+
+    floatingHearts();
+
+};
+
+// =====================================
+// CONFETTI
+// =====================================
+
+function celebrate(){
+
+    confetti({
+
+        particleCount:250,
+
+        spread:120,
+
+        startVelocity:45,
+
+        origin:{y:0.6}
+
+    });
+
+}
+
+// =====================================
+// HEARTS
+// =====================================
+
+function floatingHearts(){
+
+    for(let i=0;i<40;i++){
+
+        const heart=document.createElement("div");
+
+        heart.className="heart";
+
+        heart.innerHTML="❤️";
+
+        heart.style.left=Math.random()*window.innerWidth+"px";
+
+        heart.style.top=window.innerHeight+"px";
+
+        heart.style.animationDuration=
+        (Math.random()*2+2)+"s";
+
+        document.body.appendChild(heart);
+
+        setTimeout(()=>{
+
+            heart.remove();
+
+        },4000);
+
+    }
+
+}
